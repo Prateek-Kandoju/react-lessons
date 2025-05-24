e8f1587856a9cb01f54e536fd1d94c9f74e4f10f
Prateek-Kandoju <prateekkandoju@gmail.com>
2025-05-24 20:41:14 +0530

    UP

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,9 @@
 export default function Product(props) {
-const price = props.price + 10000
-const desc = "This is Product Description"
   return (
     <div>
-      <h3>This is Product component - {props.name},{price}</h3>
-      <p>
-        {desc}
-      </p>
+      <h3>{props.value.name}</h3>
+      <h3>{props.price}</h3>
+      <button>button</button>
     </div>
   );
 }
