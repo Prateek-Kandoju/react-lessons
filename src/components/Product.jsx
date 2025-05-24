f4d67c1473ea644a290f4629620fab96f8181b4e
Prateek-Kandoju <prateekkandoju@gmail.com>
2025-05-24 21:29:23 +0530

    UP

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,8 @@
 export default function Product(props) {
+  if (!props.value) {
+    return <div>Loading...</div>; // or return null;
+  }
+
   return (
     <div>
       <h3>{props.value.name}</h3>
